bfdf8ab8b233a00fa740994c2bab2207b3c5a656
Pierre Cattin <cattin.pierre@gmail.com>
2020-08-15 18:06:20 +0200

    Create Room Database

---
+++ app/src/main/java/com/pierrecattin/mealprep/IngredientRoomDatabase.java
@@ -0,0 +1,28 @@
+package com.pierrecattin.mealprep;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities={Ingredient.class}, version=1, exportSchema = false)
+public abstract class IngredientRoomDatabase extends RoomDatabase {
+    public abstract IngredientDao ingredientDao();
+
+    private static IngredientRoomDatabase INSTANCE;
+    private static IngredientRoomDatabase getDatabase(final Context context){
+        if(INSTANCE == null) {
+            synchronized (IngredientRoomDatabase.class){
+                if (INSTANCE== null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            IngredientRoomDatabase.class, "ingredient_database")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+}
