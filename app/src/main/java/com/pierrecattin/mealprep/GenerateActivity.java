ceafd597bbc39979ebb50e0fbf45fc3960566e42
Pierre Cattin <cattin.pierre@gmail.com>
2020-10-11 12:32:39 +0200

    MealPlan plan is attribute of GenerateActivity instead of local object

--- app/src/main/java/com/pierrecattin/mealprep/GenerateActivity.java
+++ app/src/main/java/com/pierrecattin/mealprep/GenerateActivity.java
@@ -25,6 +25,7 @@ public class GenerateActivity extends AppCompatActivity {
     private NumberPicker numberPickerMeals;
     private IngredientViewModel mIngredientViewModel;
     private List<Ingredient> ingredients;
+    private MealPlan plan;
     private ShareActionProvider shareActionProvider;
 
     @Override
@@ -59,7 +60,7 @@ public class GenerateActivity extends AppCompatActivity {
         generateMealPlan(numberPickerMeals.getValue());
     }
     public void generateMealPlan(int nbMeals) throws Exception {
-        MealPlan plan = new MealPlan();
+        plan = new MealPlan();
         plan.makePlan(nbMeals, ingredients);
         textViewMealPlan.setText(plan.toString());
         setShareActionIntent(plan.toString());
